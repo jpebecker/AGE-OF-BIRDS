0112057e6ff4cce1c7afab0f8b88aa23e93def36
jpebecker <jpebecker@gmail.com>
2022-09-19 15:02:54 -0300

    conversao 2d

---
+++ ageofbirds/Age of Birds/Assets/bird.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+       
+    }
+
+
+    private void OnMouseDown()
+    {
+        print("click");
+    }
+}
--- ageofbirds/Age of Birds/Assets/novosScripts/NewGameController.cs
+++ ageofbirds/Age of Birds/Assets/novosScripts/NewGameController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewGameController : MonoBehaviour
 {
     [Header("paineis")]
     [SerializeField] private GameObject painelTimes;
     [SerializeField] private GameObject controlBirds,controlNature;
+    [SerializeField] private Slider sliderBirds,sliderNature;
+
+    private bool TimerIsActive;
     
     void Start()
     {
@@ -25,10 +29,26 @@ public class NewGameController : MonoBehaviour
         if(team == 0)//bird
         {
             controlBirds.SetActive(true);
+            TimerIsActive = true;
+            StartSingleplayerMission(0);
         }
         else//nature
         {
             controlNature.SetActive(true);
+            TimerIsActive = true;
+            StartSingleplayerMission(1);
+        }
+    }
+
+    public void StartSingleplayerMission(int mission)
+    {
+        if (mission == 0)//bird
+        {
+          
+        }
+        else//nature
+        {
+          
         }
     }
 }
